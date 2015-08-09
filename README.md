df1bdc024ca3f6541921558c312eb1ccdd635ec0
Alex Manatskyi <aleksanderson@gmail.com>
2015-08-09 18:13:26 +0300

    Initial module

---
+++ README.md
@@ -0,0 +1,3 @@
+Amazone Kindle Vocabulary downloader
+====================================
+
---
+++ index.js
@@ -0,0 +1,36 @@
+var sqlite3 = require("sqlite3").verbose();
+var json2csv = require('json2csv');
+
+module.exports = function(config, callback) {
+
+	if(typeof arguments[0] === 'function') callback = arguments[0];
+	config = config || {};
+	config.path = config.path || getPathToKindleVocabulary();
+	config.csv = config.csv || false;
+
+	var db = new sqlite3.Database(config.path);
+
+	db.all("SELECT W.word, W.stem, L.usage from WORDS as W JOIN LOOKUPS AS L ON W.id = L.word_key", function(err, rows) {
+		if(err) return callback(err);
+
+		if(config.csv) {
+			json2csv({ data: rows, fields: ['word', 'stem', 'usage'] }, function(err, csv) {
+				if (err) return callback(err);
+				callback(null, csv);
+			});	
+		} else {
+			callback(null, rows);
+		}
+		
+	});
+
+	db.close();
+};
+
+function getPathToKindleVocabulary() {
+	switch(process.platform) {
+		case 'darwin': return '/Volumes/Kindle/system/vocabulary/vocab.db';
+		case 'linux': return ''; //TODO:
+		case 'win32': return ''; //TODO:
+	}
+}
